ee162d3e6740cc8383cc8d6efd8a5f34552851e5
Kael Lucas Pinheiro Cordeiro <kaellucaspincord@gmail.com>
2026-03-31 19:01:22 -0300

    Filtro de Notas

--- Exercícios Python Listas/ex_2_notas.py
+++ Exercícios Python Listas/ex_2_notas.py
@@ -1,12 +1,12 @@
 notas = [4.5, 7.0, 8.5, 5.0, 10, 6.5, 9.0, 3.0, 7.5, 6.0]
-aprovado = []
-reprovado = []
+aprovados = []
+reprovados = []
 
 for nota in notas:
     if nota >= 7:
-        aprovado.append(nota)
+        aprovados.append(nota)
     elif nota < 7:
-        reprovado.append(nota)
+        reprovados.append(nota)
 
-print(f"As notas dos alunos aprovados foram {aprovado}\n")
-print(f"As notas dos alunos reprovados foram {reprovado}")
+print(f"As notas dos alunos aprovados foram {aprovados}\n")
+print(f"As notas dos alunos reprovados foram {reprovados}")
