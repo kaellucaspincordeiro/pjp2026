61ac5410635675f48c78f4e349661ce2a265d031
Kael Lucas Pinheiro Cordeiro <kaellucaspincord@gmail.com>
2026-03-31 18:51:40 -0300

    Organizador de Compras

---
+++ Exercícios Python Listas/ex_1_compras.py
@@ -0,0 +1,9 @@
+compras = ["Arroz", "Feijão", "Óleo"]
+
+compras.append("Açúcar")
+
+compras.remove("Feijão")
+
+print(compras)
+
+print(f"Os itens que você tem nas suas compras são {len(compras)}")
