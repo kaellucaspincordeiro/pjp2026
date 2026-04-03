c6b5163a2c56a4a3c62dd9cbeef9079db4796cd4
Kael Lucas Pinheiro Cordeiro <kaellucaspincord@gmail.com>
2026-04-03 13:48:26 -0300

    Funções para calcular a adição, subtração, multiplicação e divisão

---
+++ Tela Calculadora/funcoes_calculadora.py
@@ -0,0 +1,14 @@
+valor_1 = 0
+valor_2 = 0
+ 
+def adicao (valor_1, valor_2):
+    return valor_1 + valor_2
+
+def subtracao (valor_1, valor_2):
+    return valor_1 - valor_2
+
+def multiplicacao (valor_1, valor_2):
+    return valor_1 * valor_2
+
+def divisao (valor_1, valor_2):
+    return valor_1 / valor_2
